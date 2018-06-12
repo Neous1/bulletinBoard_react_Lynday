cb1da332327238898ebff6acd92a2a15856b0fa7
Neous <Neous@mail.com>
2018-06-12 15:15:25 -0400

    Add constructor to board.js

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react'
 import Note from './Note'
 class Board extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            notes: [
+                {
+                    id: 33,
+                    note: "Call Lisa"
+                },
+                {
+                    id: 34,
+                    note: "email Evony"
+                }
+            ]
+        }
+    }
     render() {
         return(
             <div className ="board">

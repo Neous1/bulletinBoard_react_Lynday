07ae30af7ebd8a25ceb6eb559b383ff042877868
Neous <Neous@mail.com>
2018-06-11 21:59:48 -0400

    Import react icons

--- src/Note.js
+++ src/Note.js
@@ -1,13 +1,14 @@
 import React , {Component} from 'react'
-
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
 class Note extends Component{
     render(){
         return(
             <div className = "note">
                 <p>Learn React</p>
                 <span>
-                    <button>Edit</button>
-                    <button>Remove</button>
+                    <button><FaPencil /></button>
+                    <button><FaTrash /></button>
                 </span>
                     
             </div>
